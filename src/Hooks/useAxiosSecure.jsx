2cc999669ef498d6b5d1de377b7a249f02e1d577
Shariar <shariarnafis86@gmail.com>
2023-12-03 22:01:05 +0600

    test commit

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -5,7 +5,7 @@ import { AuthContext } from "../AuthContext/AuthProvider";
 
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://chef-place-server.vercel.app'
 })
 
 const useAxiosSecure = () => {
