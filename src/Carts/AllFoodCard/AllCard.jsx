a51c564455f3885488221e20ded8dfc9fe88b277
Shariar2006 <shariarnafis86@gmail.com>
2024-06-21 21:35:41 +0600

    add pagination meals page

--- src/Carts/AllFoodCard/AllCard.jsx
+++ src/Carts/AllFoodCard/AllCard.jsx
@@ -2,11 +2,10 @@
 
 import { useEffect, useState } from "react";
 // import useMealsCount from "../../Hooks/useMealsCount";
-import useMenu from "../../Hooks/useMenu";
 import FoodCard from "../MenuCard/FoodCard";
 
 const AllCard = () => {
-    const [menu] = useMenu()
+    // const [menu] = useMenu()
     const [meals, setMeals] = useState([]);
     const [countPage, setCountPage] = useState(0)
     const [currentPage, setCurrentPage] = useState(0)
@@ -14,34 +13,33 @@ const AllCard = () => {
     const numberOfPages = Math.ceil(countPage / mealsPerPage)
 
     const pages = [...Array(numberOfPages).keys()];
-    
 
-    useEffect(()=>{
+
+    useEffect(() => {
         fetch('http://localhost:5000/mealsCount')
-        .then(res=>res.json())
-        .then(data=>setCountPage(data.count))
-    },[])
+            .then(res => res.json())
+            .then(data => setCountPage(data.count))
+    }, [])
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch(`http://localhost:5000/allMeals?page=${currentPage}&size=${mealsPerPage}`)
-        .then(res=>res.json())
-        .then(data=>setMeals(data))
-    },[currentPage, mealsPerPage])
+            .then(res => res.json())
+            .then(data => setMeals(data))
+    }, [currentPage, mealsPerPage])
 
 
     return (
         <div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-            {
-                meals?.map(item => <FoodCard key={item._id} item={item}></FoodCard>)
-            }
-        </div>
-        <div>
-            <p>current page: {currentPage}</p>
-            {
-                pages?.map(page=><button onClick={() => {setCurrentPage(page)}} className={currentPage=== page ? "btn ml-2 mt-14 hover:text-[#EB671C] hover:bg-[#F3E6A8] text-[#EB671C] bg-[#F3E6A8]" : "btn ml-2 mt-14 bg-[#EB671C] text-[#F3E6A8] hover:text-[#EB671C] hover:bg-[#F3E6A8]"} key={page}>{page}</button>)
-            }
-        </div>
+                {
+                    meals?.map(item => <FoodCard key={item._id} item={item}></FoodCard>)
+                }
+            </div>
+            <div>
+                {
+                    pages?.map(page => <button onClick={() => { setCurrentPage(page) }} className={currentPage === page ? "btn ml-2 mt-14 hover:text-[#EB671C] hover:bg-[#F3E6A8] text-[#EB671C] bg-[#F3E6A8]" : "btn ml-2 mt-14 bg-[#EB671C] text-[#F3E6A8] hover:text-[#EB671C] hover:bg-[#F3E6A8]"} key={page}>{page}</button>)
+                }
+            </div>
         </div>
     );
 };
--- src/Components/Pages/AllMeals/AllMeals.jsx
+++ src/Components/Pages/AllMeals/AllMeals.jsx
@@ -1,12 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import FoodCard from "../../../Carts/MenuCard/FoodCard";
-import useMenu from "../../../Hooks/useMenu";
-// import InfiniteScroll from 'react-infinite-scroll-component';
-
-
 
 const AllMeals = () => {
     const [search, setSearch] = useState('')
+    const [meals, setMeals] = useState([]);
+    const [countPage, setCountPage] = useState(0)
+    const [currentPage, setCurrentPage] = useState(0)
+    const mealsPerPage = 9;
+    const numberOfPages = Math.ceil(countPage / mealsPerPage)
+
+    const pages = [...Array(numberOfPages).keys()];
+
+    useEffect(() => {
+        fetch('http://localhost:5000/mealsCount')
+            .then(res => res.json())
+            .then(data => setCountPage(data.count))
+    }, [])
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/allMeals?page=${currentPage}&size=${mealsPerPage}`)
+            .then(res => res.json())
+            .then(data => setMeals(data))
+    }, [currentPage, mealsPerPage])
 
     const handleSearch = e => {
         e.preventDefault()
@@ -17,28 +32,8 @@ const AllMeals = () => {
 
     console.log(search)
 
-    // const [dataSource, setDataSource] = useState(Array.from({ length: 6 }))
-
-    const [menu] = useMenu()
     return (
         <div className="pt-36">
-            {/* <InfiniteScroll
-                dataLength={dataSource.length}
-                // // next={menu}
-                // hasMore={true}
-                loader={<h4>Loading...</h4>}
-            // endMessage={
-            //   <p style={{ textAlign: 'center' }}>
-            //     <b>Yay! You have seen it all</b>
-            //   </p>
-            // }
-            >
-                {
-                    dataSource.map((scrollItem) => {
-                        return 
-                    })
-                }
-            </InfiniteScroll> */}
 
             <form onChange={handleSearch} className="p-2">
                 <input className="border-2 w-full my-2 p-2 rounded-md" placeholder="Search" type="search" name="" id="">
@@ -47,10 +42,15 @@ const AllMeals = () => {
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
                 {
-                    menu?.filter((menuItem) => {
+                    meals?.filter((menuItem) => {
                         return search.toLocaleLowerCase() === '' ? menuItem : menuItem?.category.toLocaleLowerCase().includes(search) || menuItem?.name.toLocaleLowerCase().includes(search) 
                         // || parseInt(menuItem?.price).includes(search)
                     }).map(item => <FoodCard key={item._id} item={item}></FoodCard>)
+                }
+            </div>
+            <div className="text-center">
+                {
+                    pages?.map(page => <button onClick={() => { setCurrentPage(page) }} className={currentPage === page ? "btn ml-2 my-14 hover:text-[#EB671C] hover:bg-[#F3E6A8] text-[#EB671C] bg-[#F3E6A8]" : "btn ml-2 my-14 bg-[#EB671C] text-[#F3E6A8] hover:text-[#EB671C] hover:bg-[#F3E6A8]"} key={page}>{page}</button>)
                 }
             </div>
 
